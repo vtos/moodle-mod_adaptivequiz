1edcedd46e07233e418253a49ec72ed5742008fa
Vitaly Potenko <potenkov@gmail.com>
2022-01-13 12:36:01 +0300

    Fixed all unit tests for the lib test case.

--- tests/lib_test.php
+++ tests/lib_test.php
@@ -1,25 +1,27 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is not a part of Moodle - http://moodle.org/.
+// This is a non-core contributed module. The module had been created
+// as a collaborative effort between Middlebury College and Remote Learner.
+// Later on it was adopted by a developer Vitaly Potenko to keep it compatible
+// with new Moodle versions and let it acquire new features.
 //
-// Moodle is free software: you can redistribute it and/or modify
+// This is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// This is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// The GNU General Public License can be seen at <http://www.gnu.org/licenses/>.
 
 /**
  * Adaptive lib.php PHPUnit tests
  *
- * @package    mod_adaptivequiz
- * @category   phpunit
- * @copyright  2013 onwards Remote-Learner {@link http://www.remote-learner.ca/}
+ * @copyright  2013 Remote-Learner {@link http://www.remote-learner.ca/}
+ * @copyright  2022 onwards Vitaly Potenko <potenkov@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -37,7 +39,9 @@ class mod_adaptivequiz_lib_testcase extends advanced_testcase {
      * @return void
      */
     protected function setup_test_data_xml() {
-        $this->loadDataSet($this->createXMLDataSet(__DIR__.'/fixtures/mod_adaptivequiz.xml'));
+        $this->dataset_from_files(
+            [__DIR__.'/fixtures/mod_adaptivequiz.xml']
+        )->to_database();
     }
 
     /**
@@ -173,14 +177,13 @@ class mod_adaptivequiz_lib_testcase extends advanced_testcase {
         $dummy->cmid = 99;
 
         $output = adaptivequiz_print_recent_mod_activity($dummy, 1, true, array('mod_adaptivequiz' => 'adaptivequiz'), true, true);
-
-        $this->assertContains('<table', $output);
-        $this->assertContains('<tr>', $output);
-        $this->assertContains('<td', $output);
-        $this->assertContains('mod/adaptivequiz/view.php?id=99', $output);
-        $this->assertContains('/user/view.php?id=2', $output);
-        $this->assertContains('user phpunit', $output);
-        $this->assertContains('my-phpunit-test', $output);
+        $this->assertStringContainsString('<table', $output);
+        $this->assertStringContainsString('<tr>', $output);
+        $this->assertStringContainsString('<td', $output);
+        $this->assertStringContainsString('mod/adaptivequiz/view.php?id=99', $output);
+        $this->assertStringContainsString('/user/view.php?id=2', $output);
+        $this->assertStringContainsString('user phpunit', $output);
+        $this->assertStringContainsString('my-phpunit-test', $output);
     }
 
     /**
@@ -212,10 +215,10 @@ class mod_adaptivequiz_lib_testcase extends advanced_testcase {
 
         $output = adaptivequiz_print_recent_mod_activity($dummy, 1, false, array('mod_adaptivequiz' => 'adaptivequiz'), true, true);
 
-        $this->assertContains('<table', $output);
-        $this->assertContains('<tr>', $output);
-        $this->assertContains('<td', $output);
-        $this->assertContains('/user/view.php?id=2', $output);
-        $this->assertContains('user phpunit', $output);
+        $this->assertStringContainsString('<table', $output);
+        $this->assertStringContainsString('<tr>', $output);
+        $this->assertStringContainsString('<td', $output);
+        $this->assertStringContainsString('/user/view.php?id=2', $output);
+        $this->assertStringContainsString('user phpunit', $output);
     }
 }

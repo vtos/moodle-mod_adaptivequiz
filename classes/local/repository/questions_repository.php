8230e0918a58cca12ec987a014a7fd03a23096f1
Vitaly Potenko <potenkov@gmail.com>
2022-01-29 14:13:16 +0300

    Introduced questions repository.

---
+++ classes/local/repository/questions_repository.php
@@ -0,0 +1,33 @@
+<?php
+// This file is not a part of Moodle - http://moodle.org/.
+// This is a non-core contributed module. The module had been created
+// as a collaborative effort between Middlebury College and Remote Learner.
+// Later on it was adopted by a developer Vitaly Potenko to keep it compatible
+// with new Moodle versions and let it acquire new features.
+//
+// This is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// The GNU General Public License can be seen at <http://www.gnu.org/licenses/>.
+
+/**
+ * A class to wrap all database queries which are specific to questions and their related data. Normally should contain
+ * only static methods to call.
+ *
+ * @copyright  2022 onwards Vitaly Potenko <potenkov@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_adaptivequiz\local\repository;
+
+final class questions_repository
+{
+    
+}

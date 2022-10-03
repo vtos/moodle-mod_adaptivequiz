f824f70ac4bcd1187c398e01f7487618de0b4292
vtos <potenkov@gmail.com>
2022-10-03 15:17:22 +0200

    Updated version info to indicate work in progress.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2022092600;
-$plugin->release = '2.2.1';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.3.1dev';
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2022041900;
 $plugin->cron = 0;
 $plugin->component = 'mod_adaptivequiz';

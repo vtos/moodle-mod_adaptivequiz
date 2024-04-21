f633741054bef18582adcc8d3ca61f30bf316409
vtos <potenkov@gmail.com>
2024-04-21 13:29:12 +0200

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024042000;
+$plugin->version = 2024042100;
 $plugin->release = '3.0.0dev';
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2024042200;

9f67ad57bade40356a077a3fc15bece0c2469e12
vtos <potenkov@gmail.com>
2024-08-26 14:21:50 +0200

    CAT model: add delete attempt callback
    Allowed sub-plugin to run their code immediately after an attempt is deleted.
    Note: the added code should not be inside the deletion
    script, it'll be moved out eventually.

--- delattempt.php
+++ delattempt.php
@@ -17,6 +17,7 @@
 /**
  * Confirmation page to remove student attempts.
  *
+ * @package    mod_adaptivequiz
  * @copyright  2013 onwards Remote-Learner {@link http://www.remote-learner.ca/}
  * @copyright  2022 onwards Vitaly Potenko <potenkov@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -26,6 +27,7 @@ require_once(__DIR__ . '/../../config.php');
 
 $attemptid = required_param('attempt', PARAM_INT);
 $confirm = optional_param('confirm', 0, PARAM_INT);
+$returnurl = optional_param('return', '', PARAM_URL);
 
 $attempt = $DB->get_record('adaptivequiz_attempt', ['id' => $attemptid], '*', MUST_EXIST);
 $adaptivequiz = $DB->get_record('adaptivequiz', ['id' => $attempt->instance], '*', MUST_EXIST);
@@ -45,15 +47,36 @@ $PAGE->set_title(format_string($adaptivequiz->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($context);
 
-$returnurl = new moodle_url('/mod/adaptivequiz/viewattemptreport.php', ['cmid' => $cm->id, 'userid' => $user->id]);
-
 $a = new stdClass();
 $a->name = fullname($user);
 $a->timecompleted = userdate($attempt->timemodified);
 
+$customcatmodelinuse = !empty($adaptivequiz->catmodel);
+
+// Handle the case when a custom CAT model is in use, but return URL wasn't provided. Redirect to the plugin's view page then.
+if ($customcatmodelinuse && !$returnurl) {
+    $returnurl = new moodle_url('/mod/adaptivequiz/view.php', ['id' => $cm->id]);
+}
+
+if (!$customcatmodelinuse) {
+    $returnurl = new moodle_url('/mod/adaptivequiz/viewattemptreport.php', ['cmid' => $cm->id, 'userid' => $user->id]);
+}
+
 if ($confirm) {
     question_engine::delete_questions_usage_by_activity($attempt->uniqueid);
     $DB->delete_records('adaptivequiz_attempt', ['id' => $attempt->id]);
+
+    // Allow sub-plugins to run their code when relevant.
+    if ($customcatmodelinuse) {
+        $catmodelcomponentname = 'adaptivequizcatmodel_' . $adaptivequiz->catmodel;
+        $pluginswithfunction = get_plugin_list_with_function('adaptivequizcatmodel', 'post_delete_attempt_callback');
+        if (!array_key_exists($catmodelcomponentname, $pluginswithfunction)) {
+            return;
+        }
+
+        $functionname = $pluginswithfunction[$catmodelcomponentname];
+        $functionname($adaptivequiz, $attempt);
+    }
 
     adaptivequiz_update_grades($adaptivequiz, $user->id);
 
@@ -63,7 +86,9 @@ if ($confirm) {
 
 $message = get_string('confirmdeleteattempt', 'adaptivequiz', $a);
 
-$confirm = new moodle_url('/mod/adaptivequiz/delattempt.php', ['attempt' => $attempt->id, 'confirm' => 1]);
+$confirm = new moodle_url('/mod/adaptivequiz/delattempt.php',
+    ['attempt' => $attempt->id, 'confirm' => 1, 'return' => $returnurl]);
+
 echo $OUTPUT->header();
 echo $OUTPUT->confirm($message, $confirm, $returnurl);
 echo $OUTPUT->footer();
